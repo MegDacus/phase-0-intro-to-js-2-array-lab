0e2b2765950b33adc9b347b191774607ad3b9b39
Megan Dacus <meganjustice0609@gmail.com>
2023-03-13 21:59:11 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newCats = [...cats.slice(0,cats.length), name]
+    return newCats
+}
+
+function prependCat(name) {
+    const newerCats = [name, ...cats.slice(0,cats.length)]
+    return newerCats
+}
+
+function removeLastCat() {
+    const fewCats = cats.slice(0,2)
+    return fewCats
+}
+
+function removeFirstCat() {
+    const fewerCats = cats.slice(1,3)
+    return fewerCats
+}
